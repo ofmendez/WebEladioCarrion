51a81ca295c3f2cdb703079f7b5f01d3749a8605
Andres Garcia <andresgarcia@tretto.co>
2025-07-26 17:13:49 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementById('btn-tour').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const videoPC = document.getElementById('video-pc');
+  const videoMobile = document.getElementById('video-mobile');
+
+  const newVideoURL = 'https://antesdeltour.com/Videos/Eladioconcerts.mp4';
+  const newPosterPC = 'assets/img/PosterTourDesktop.webp';
+  const newPosterMobile = 'assets/img/PosterTourMobile.webp';
+
+  // Actualizar video PC
+  const pcSource = videoPC.querySelector('source');
+  pcSource.src = newVideoURL;
+  videoPC.setAttribute('poster', newPosterPC);
+  videoPC.load();
+  videoPC.play();
+
+  // Actualizar video móvil
+  const mobileSource = videoMobile.querySelector('source');
+  mobileSource.src = newVideoURL;
+  videoMobile.setAttribute('poster', newPosterMobile);
+  videoMobile.load();
+  videoMobile.play();
+
+  // Ocultar tarjeta del menú con animación
+  const menuCard = document.querySelector('.menu-card');
+  if (menuCard) {
+    menuCard.classList.add('hide');
+  }
+
+
+  // Mostrar overlay
+document.querySelector('.menu-card').classList.add('hide');
+document.getElementById('tour-overlay').classList.remove('hidden');
+
+// Botón HOME: recarga la página
+document.getElementById('btn-home').addEventListener('click', () => {
+  window.location.reload();
+});
+
+
+});
+
+
+const regionButtons = document.querySelectorAll('.region-btn');
+const allTables = document.querySelectorAll('.region-table');
+
+regionButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const region = btn.textContent.trim().toLowerCase();
+
+    allTables.forEach(table => table.classList.remove('active'));
+
+    const targetTable = document.getElementById(`table-${region}`);
+    if (targetTable) {
+      targetTable.classList.add('active');
+    }
+  });
+});
